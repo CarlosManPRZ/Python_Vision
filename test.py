ffb8a81c16e5cf11ca27b2156716a9a237d363a3
CarlosManPRZ <111636570+CarlosManPRZ@users.noreply.github.com>
2022-08-23 13:25:10 -0500

    Primer paso
    hola

---
+++ test.py
@@ -0,0 +1,13 @@
+#Primer ejemplo de python + Open CV en VSCode
+
+#importar librerias
+import numpy
+import cv2
+import matplotlib.pyplot as plt
+
+#Crear objeto de imagen
+guacamaya_im = cv2.imread("Guacamaya.jpg")
+plt.title("Imagen cargada sin invertir colores BGR")
+#plt.xlabel ('Dimensión de imagen: ' + str(guacamaya_im.shape))
+plt.imshow(guacamaya_im)
+plt.show()
